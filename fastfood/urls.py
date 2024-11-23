cef7b126701e415ddb4e953fdaf8fb7306b7cf45
temirovuz <temirov-muhammad@mail.ru>
2024-11-23 15:50:06 +0500

    url update

--- fastfood/urls.py
+++ fastfood/urls.py
@@ -4,10 +4,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from fastfood.views import CreateFood, FoodRetrieveUpdateDestroyView, OrderListView, OrderCreateView
 
 urlpatterns = [
-    path('foods/', CreateFood.as_view(), name='dish-list-create'),
-    path('foods/<int:pk>/', FoodRetrieveUpdateDestroyView.as_view(), name='dish-detail'),
+    path('foods/', CreateFood.as_view(), name='food-list-create'),
+    path('foods/<int:pk>/', FoodRetrieveUpdateDestroyView.as_view(), name='food-detail'),
     path('orders/', OrderListView.as_view(), name='order-list'),
     path('orders/create/', OrderCreateView.as_view(), name='order-create'),
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
+
